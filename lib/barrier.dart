d2ce78b7d6b24be0d00fbe01984a23a2d09200f4
Mykyta Zviahin <mykytazviahin@yahoo.com>
2020-11-19 22:51:54 +0200

    add features

--- lib/barrier.dart
+++ lib/barrier.dart
@@ -13,6 +13,8 @@ class MyBarrier extends StatelessWidget {
       height: size,
       decoration: BoxDecoration(
         color: Colors.green,
+        border: Border.all(width: 10, color: Colors.green[800]),
+        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -78,8 +78,13 @@ class _HomePageState extends State<HomePage> {
                       ),
                   ),
                 ),
-                MyBarrier(
-                  size: 200.0,
+                AnimatedContainer(
+                  alignment: Alignment(0, 1),
+                  duration: Duration(milliseconds: 0),
+                  child: MyBarrier(
+                      
+                      size: 200.0,
+                  ),
                 ),
               ],
             ),
